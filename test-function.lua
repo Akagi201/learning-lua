4a01e21348e4dcdd25e7cd5996698d976d56dd3b
Akagi201 <akagi201@gmail.com>
2014-06-11 20:45:07 +0800

    add: add test-function.lua

---
+++ test-function.lua
@@ -0,0 +1,25 @@
+
+-- 在 Lua 中，函数是被看作是“第一类值（First-Class Value）”，函数可以存在变量里
+
+function factorial1(n)
+    if n == 0 then
+        return 1
+    else
+        return n * factorial1(n - 1)
+    end
+end
+print(factorial1(5))
+factorial2 = factorial1
+print(factorial2(5))
+
+-- function 可以以匿名函数（anonymous function）的方式通过参数传递
+
+function anonymous(tab, fun)
+    for k, v in pairs(tab) do
+        print(fun(k, v))
+    end
+end
+tab = { key1 = "val1", key2 = "val2" }
+anonymous(tab, function(key, val)
+    return key .. " = " .. val
+end)
