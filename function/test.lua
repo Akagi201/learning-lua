e33b844d7ed1db772ceb884c6ca9e91fe1a85adb
Akagi201 <akagi201@gmail.com>
2015-11-24 16:21:35 +0800

    add module function

--- function/test.lua
+++ function/test.lua
@@ -6,5 +6,10 @@ local function doAction(method, ...)
     method(unpack(args, 1, table.maxn(args)))
 end
 
+print(type(sample))
+for k, v in pairs(sample) do
+    print(k, v)
+end
+
 doAction(sample.hello, sample, ' 123') -- 相当于sample:hello('123')
 doAction(sample.world, ' 321') -- 相当于sample.world('321')
